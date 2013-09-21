48f19335c9c8c329a0d2f843ba4d327b6737b4a2
Radford Smith <contact@radsmith.com>
2013-09-21 01:56:12 -0700

    Completed "Making a Counter"

---
+++ tutorial-client/app/src/tutorial_client/behavior.clj
@@ -0,0 +1,14 @@
+(ns ^:shared tutorial-client.behavior
+    (:require [clojure.string :as string]
+              [io.pedestal.app.messages :as msg]))
+
+(defn set-value-transform [old-value message]
+  (:value message))
+
+(defn inc-transform [old-value _]
+  ((fnil inc 0) old-value))
+
+(def example-app
+  {:version 2
+   :transform [[:inc [:my-counter] inc-transform]]})
+
